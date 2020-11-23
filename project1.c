058c0acf5d62b1d2309110feda92de5d1c2e34e0
seongjin Kang <rkdtjdwls177@knu.ac.kr>
2020-11-23 20:20:24 +0900

    f2() introduce seongjin

--- project1.c
+++ project1.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 void f1();
+void f2();
 int main()	
 {
 	f1();
+	f2();
 	return 0;
 }
 
 void f1()
 {
 	printf("first commit!\n");
+}
+
+void f2()
+{
+	printf("hello my name is seongjin kang\n");
 }
